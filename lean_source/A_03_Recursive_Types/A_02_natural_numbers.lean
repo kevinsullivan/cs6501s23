18ef97f46bf41269e707854287523b8448a4a58d
Kevin Sullivan <sullivan.kevinj@gmail.com>
2023-03-02 05:24:09 +0000

    Update notes

--- lean_source/A_03_Recursive_Types/A_02_natural_numbers.lean
+++ lean_source/A_03_Recursive_Types/A_02_natural_numbers.lean
@@ -3,12 +3,16 @@
 ***************
 Natural Numbers
 ***************
+
+Data Type
+---------
 TEXT. -/
 
 -- QUOTE:
 #check nat
+-- QUOTE.
 
-/-
+/- TEXT
 The definition of the type, nat, the terms/values of which we
 use to represent natural numbers.
 
@@ -19,16 +23,19 @@ inductive nat
 The set of terms is defined inductively. First, *zero* is a term
 of type *nat*. Second, if *n'* is any term of type *nat*, then so
 is *nat.succ n'* (which you can also write as n'.succ or n' + 1).
--/
+TEXT. -/
 
+-- QUOTE: 
 -- notations for writing succ applications
 def three'  := (nat.succ (nat.succ (nat.succ (nat.zero))))
 def three  := nat.zero.succ.succ.succ
-
 -- QUOTE.
 
 
 /- TEXT:
+Operations
+----------
+
 Having seen how the *nat* data type is defined, we now look 
 at how to define functions taking *nat* values as arguments.
 As we've seen before, many such functions here will again be
@@ -45,8 +52,9 @@ TEXT. -/
 -- increment is just succ application 
 def inc (n' : nat) : nat := n'.succ
 def three'' := inc(inc(inc nat.zero))
+-- QUOTE.
 
-/-
+/- TEXT:
 A predecessor (one less than) function can be defined by 
 case analysis on a nat argument. Here we'll define *pred'*
 to return 0 when applied to 0, and otherwise to return the
@@ -57,56 +65,53 @@ Rather than "implementing a function" think "proving a function
 type." A "proof" of function type, *nat → nat,* is any function 
 that converts any given nat into some resulting nat. 
 
-When proving a function or other data type, as opposed to a 
-logical (proposition) type, is that *any* proof will do to prove
-the proposition, while we usually want a specific function of the
-specified type. Here, for example, we don't want any function
-that takes and returns a nat, but one that in particular returns
-the required answer (one less than the argument).
+When proving a proposition (a type in Prop), any proof (value
+of that type) will do. When proving function or other data type, 
+however, the particular value of the type that you construct is
+usually important. Here, for example, we don't want any function
+that takes and returns a nat, but one that returns the right nat
+for the given argument.
+TEXT. -/
 
-Differences between proofs of propositions are irrelevant in
-Lean. Differences between "proofs" of function or other data
-types aren't irrelevant; they're usually highly relevant! So 
-be sure to construct a preferred "proof" of any given function
-type.
-
-Let's look at the predecessor function: the one that takes any
-nat value and returns the following: zero if the argument is 
-zero, and otherwise n', where the argument value is n'+1. 
--/
+-- QUOTE:
 def pred' : nat → nat :=
 begin
 assume n,
-cases n with n',  -- this is right now
-exact 0,
-exact n',
+cases n with n',  
+exact 0,  -- when n is zero
+exact n', -- when n is succ n'
 end 
 
 -- quick test
 #eval pred' 6
 example : pred' 6 = 5 := rfl
+-- QUOTE.
 
-/-
+/- TEXT:
 Here's the same function just specified
 using pattern matching notation (which,
 as we've seen generalizes case analysis). 
- -/
+TEXT. -/
+
+-- QUOTE: 
 def pred : nat → nat 
 | nat.zero := nat.zero  -- loop at zero
 | (nat.succ n') := n' 
 
 #eval pred 5
 example : pred 5 = 4 := rfl
+-- QUOTE.
 
-/-
+/- TEXT:
 Pattern matching generalizes case analysis
 by giving you a means to return different
 results based on deeper analysis of argument
 structures using pattern matching/unification.
--/
+TEXT. -/
+
+-- QUOTE:
 def sub2 : nat → nat 
 | nat.zero := nat.zero
 | (nat.succ nat.zero) := nat.zero
 | (nat.succ (nat.succ n')) := n'
-
 -- QUOTE.
--- src/A_03_Recursive_Types/A_02_natural_numbers.lean
+++ src/A_03_Recursive_Types/A_02_natural_numbers.lean
@@ -2,7 +2,7 @@
 
 #check nat
 
-/-
+/- TEXT
 The definition of the type, nat, the terms/values of which we
 use to represent natural numbers.
 
@@ -13,7 +13,6 @@ inductive nat
 The set of terms is defined inductively. First, *zero* is a term
 of type *nat*. Second, if *n'* is any term of type *nat*, then so
 is *nat.succ n'* (which you can also write as n'.succ or n' + 1).
--/
 
 -- notations for writing succ applications
 def three'  := (nat.succ (nat.succ (nat.succ (nat.zero))))
@@ -21,56 +20,24 @@ def three  := nat.zero.succ.succ.succ
 
 
 
-
 -- increment is just succ application 
 def inc (n' : nat) : nat := n'.succ
 def three'' := inc(inc(inc nat.zero))
 
-/-
-A predecessor (one less than) function can be defined by 
-case analysis on a nat argument. Here we'll define *pred'*
-to return 0 when applied to 0, and otherwise to return the
-one smaller value, *n'*, when applied to any non-zero value,
-*nat.succ n'*. 
 
-Rather than "implementing a function" think "proving a function 
-type." A "proof" of function type, *nat → nat,* is any function 
-that converts any given nat into some resulting nat. 
-
-When proving a function or other data type, as opposed to a 
-logical (proposition) type, is that *any* proof will do to prove
-the proposition, while we usually want a specific function of the
-specified type. Here, for example, we don't want any function
-that takes and returns a nat, but one that in particular returns
-the required answer (one less than the argument).
-
-Differences between proofs of propositions are irrelevant in
-Lean. Differences between "proofs" of function or other data
-types aren't irrelevant; they're usually highly relevant! So 
-be sure to construct a preferred "proof" of any given function
-type.
-
-Let's look at the predecessor function: the one that takes any
-nat value and returns the following: zero if the argument is 
-zero, and otherwise n', where the argument value is n'+1. 
--/
 def pred' : nat → nat :=
 begin
 assume n,
-cases n with n',  -- this is right now
-exact 0,
-exact n',
+cases n with n',  
+exact 0,  -- when n is zero
+exact n', -- when n is succ n'
 end 
 
 -- quick test
 #eval pred' 6
 example : pred' 6 = 5 := rfl
 
-/-
-Here's the same function just specified
-using pattern matching notation (which,
-as we've seen generalizes case analysis). 
- -/
+
 def pred : nat → nat 
 | nat.zero := nat.zero  -- loop at zero
 | (nat.succ n') := n' 
@@ -78,14 +45,8 @@ def pred : nat → nat
 #eval pred 5
 example : pred 5 = 4 := rfl
 
-/-
-Pattern matching generalizes case analysis
-by giving you a means to return different
-results based on deeper analysis of argument
-structures using pattern matching/unification.
--/
+
 def sub2 : nat → nat 
 | nat.zero := nat.zero
 | (nat.succ nat.zero) := nat.zero
 | (nat.succ (nat.succ n')) := n'
-
